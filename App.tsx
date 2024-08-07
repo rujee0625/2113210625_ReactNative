4258cfddba979ba6d9578e960604b79881114287
rujee0625 <rujeenakkardpanou@gmail.com>
2024-08-07 10:30:33 +0700

    UseEffect Practice

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { StyleSheet, Text, TextInput, View, ComponentProvider, Alert} from "react-native";
 import ProfileScreen from "./components/ProfileScreen";
 import Login from "./components/Login";
+import ExampleEffect from "./components/ExampleEffect";
 
 export default function App(): React.JSX.Element {
   
   return (
     <View >     
-      <ProfileScreen/>
-      <Login/>
+      <ExampleEffect/>
     </View>
   );
 }
--- components/ExampleEffect.tsx
+++ components/ExampleEffect.tsx
@@ -12,16 +12,18 @@ const ExampleEffect = () => {
 
   useEffect(() =>{
     console.log("use Effect2 - Runs only when count1 change")
-  });
+  },[count1]);
 
   useEffect(() =>{
     console.log("use Effect3 - Runs only once after the first render")
-  });
+  },[]);
   
   return (
     <View style={{marginTop: 50}}>
         <Text style={{ marginTop: 50}}>Count1: {count1}</Text>
         <Button title="Increment Count1" onPress={() => setCount1(count1 + 1)}/>
+        <Text>{"\n"}</Text>
+        <Text>{"\n"}</Text>
         <Text>Count2: {count2}</Text>
         <Button title="Increment Count2" onPress={() => setCount2(count2 + 1)}/>
     </View>
